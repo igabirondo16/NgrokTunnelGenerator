eb7cddeb207ca309dce943c9538282b688497b7d
Iñigo Gabirondo <inigo.ikasle@ehu.eus>
2021-07-22 15:18:46 +0200

    Add NgrokTunnelGenerator.py file

---
+++ NgrokTunnelGenerator.py
@@ -0,0 +1,54 @@
+import traceback
+from pyngrok import ngrok
+import re
+import sys
+
+DEFAULT_PATH = ""
+
+def open_tunnel():
+    ssh_tunnel = ngrok.connect(5005, "http")
+    
+    for t in ngrok.get_tunnels():
+        if "https" in t.public_url:
+            tunnel = t.public_url
+
+    with open(DEFAULT_PATH, "r+") as file:
+        file_contents = file.read()
+        text_pattern = re.compile('http[s]?://[A-Za-z0-9_]*\.ngrok\.io', 0)
+        file_contents = text_pattern.sub(tunnel, file_contents)
+        file.seek(0)
+        file.truncate()
+        file.write(file_contents)
+        file.close()
+
+    print("Credentials.yml file updated")
+    print("Ngrok server started at: " + tunnel)
+
+    try:
+        ngrok_process = ngrok.get_ngrok_process()
+        ngrok_process.proc.wait()
+
+    except KeyboardInterrupt:
+        print("Shutting down server at: " + str(tunnel))
+
+
+def update_path():
+    global DEFAULT_PATH
+    DEFAULT_PATH = str(input("Select path of credentials.yml file: "))
+
+def show_menu():
+    print("NGROK TUNNEL OPENER")
+    print("1) Update the path of credentials.yml file: ")
+    print("2) Open tunnel for selected file")
+
+if __name__ == "__main__":
+    try: 
+        show_menu()
+        choice = int(input("Select your choice: "))
+        if choice == 1:
+            update_path()
+
+        open_tunnel()
+
+    except Exception as e:
+        print(traceback.format_exc())
